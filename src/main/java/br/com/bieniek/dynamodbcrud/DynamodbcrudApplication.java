033183906be1eca48538c22987dd6f871d57caa6
Bianeck <thiagobianeck@gmail.com>
2023-04-01 12:29:01 -0300

    Commit inicial do projeto

---
+++ src/main/java/br/com/bieniek/dynamodbcrud/DynamodbcrudApplication.java
@@ -0,0 +1,13 @@
+package br.com.bieniek.dynamodbcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamodbcrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamodbcrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/bieniek/dynamodbcrud/DynamodbcrudApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bieniek.dynamodbcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamodbcrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
